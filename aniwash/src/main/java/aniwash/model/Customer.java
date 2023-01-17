1ac2a36e497291e9aa8183e1c06942335146138e
lassib <91030337+lassib@users.noreply.github.com>
2023-01-17 14:37:33 +0200

    Setters and getters

--- aniwash/src/main/java/aniwash/model/Customer.java
+++ aniwash/src/main/java/aniwash/model/Customer.java
@@ -1,29 +1,71 @@
 package aniwash.model;
 
-public class Customer implements IUser{
+public class Customer implements IUser {
+
+    private String name;
+    private int id;
+    private String phone;
+    private String email;
+    private String address;
+    private String postalcode;
 
     @Override
-    public void createUser() {
-        // TODO Auto-generated method stub
-        
+    public String getName() {
+        return name;
     }
 
     @Override
-    public void deleteUser() {
-        // TODO Auto-generated method stub
-        
+    public int getId() {
+        return id;
     }
 
     @Override
-    public void updateUser() {
-        // TODO Auto-generated method stub
-        
+    public String getPhone() {
+        return phone;
     }
 
     @Override
-    public void getUser() {
-        // TODO Auto-generated method stub
-        
+    public String getEmail() {
+        return email;
     }
-    
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String getPostalcode() {
+        return postalcode;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public void setPostalcode(String postalcode) {
+        this.postalcode = postalcode;
+    }
 }
--- aniwash/src/main/java/aniwash/model/IUser.java
+++ aniwash/src/main/java/aniwash/model/IUser.java
@@ -1,9 +1,18 @@
 package aniwash.model;
 
 public interface IUser {
-    
-    public void createUser();
-    public void deleteUser();
-    public void updateUser();
-    public void getUser();
+
+    public String getName();
+    public int getId();
+    public String getPhone();
+    public String getEmail();
+    public String getAddress();
+    public String getPostalcode();
+    public void setName(String name);
+    public void setId(int id);
+    public void setPhone(String phone);
+    public void setEmail(String email);
+    public void setAddress(String address);
+    public void setPostalcode(String postalcode);
+
 }
