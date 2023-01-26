d50fd149756d7ae5a7f40b78498e187167e75029
Jonne Borgman <103989050+Cl0wnFiesta@users.noreply.github.com>
2023-01-26 09:11:07 +0200

    Launchable launcher (#1)
    * s
    
    * removes warnings

--- aniwash/src/main/java/aniwash/App.java
+++ aniwash/src/main/java/aniwash/App.java
@@ -1,13 +1,15 @@
 package aniwash;
 
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        aniwash.MainApp.startSimulation(args); 
     }
 }
