223776f378f6c1a7dd6a6f2bb03f3d673ea4908a
Rasmus Hyyppä <rasmus.hyyppa@metropolia.fi>
2023-02-03 15:26:35 +0200

    latest master merged, changed Employee class for now to not include address and postcode

--- aniwash/src/main/java/aniwash/entity/Employee.java
+++ aniwash/src/main/java/aniwash/entity/Employee.java
@@ -3,15 +3,14 @@ package aniwash.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "Employee")
 public class Employee extends User {
     String title;
 
     public Employee() {
     }
 
-    public Employee(String name, int id, String title, String phone, String email, String address, String postalcode) {
-        super(name, id, phone, email, address, postalcode);
+    public Employee(String name, int id, String title, String phone, String email) {
+        super(name, id, phone, email);
         this.title = title;
     }
 }
