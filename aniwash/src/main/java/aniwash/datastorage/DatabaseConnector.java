ec893b382e929a0eb45244b8623eaf958a5df348
Rasmus Hyyppä <rasmus.hyyppa@metropolia.fi>
2023-03-03 13:34:28 +0200

    Jenkins fix

--- aniwash/src/main/java/aniwash/datastorage/DatabaseConnector.java
+++ aniwash/src/main/java/aniwash/datastorage/DatabaseConnector.java
@@ -1,6 +1,12 @@
 package aniwash.datastorage;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DatabaseConnector {
 
@@ -10,7 +16,14 @@ public class DatabaseConnector {
     public static EntityManager getInstance() {
         if (em == null) {
             if (emf == null) {
-                emf = Persistence.createEntityManagerFactory("com.aniwash");
+                Properties persistenceProperties = new Properties();
+                try {
+                    persistenceProperties.load(new FileInputStream("./aniwash/persistence.properties"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                emf = Persistence.createEntityManagerFactory("com.aniwash", persistenceProperties);
             }
             em = emf.createEntityManager();
         }
