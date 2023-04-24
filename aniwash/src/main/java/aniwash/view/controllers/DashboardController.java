15915a9456347db8a7d7afaff7b3d9565b491c39
henriui <henri.uimonen@gmail.com>
2023-04-24 20:52:43 +0300

    wörks

--- aniwash/src/main/java/aniwash/view/controllers/DashboardController.java
+++ aniwash/src/main/java/aniwash/view/controllers/DashboardController.java
@@ -17,7 +17,7 @@ import javafx.scene.text.Text;
 import java.io.IOException;
 import java.time.Month;
 import java.util.Calendar;
-import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Locale;
 
@@ -31,11 +31,15 @@ public class DashboardController {
     @FXML
     private Button scheduleButton;
     @FXML
-    private BarChart<String, Double> barChart;
+    private BarChart<Month, Double> barChart;
     @FXML
     private Text thisMonth;
     @FXML
-    private Text thisYear;
+    private Text lastMonth;
+    @FXML
+    private Text customersThisMo;
+    @FXML
+    private Text customersThisYear;
 
     private final MainViewModel mainViewModel = new MainViewModel();
     @FXML
@@ -108,52 +112,66 @@ public class DashboardController {
     }
 
     private void loadDataHistory() {
-        Calendar now = Calendar.getInstance();
+        Calendar cal = Calendar.getInstance();
         AppointmentDao apptDao = new AppointmentDao();
-        HashMap<Month, Double> monthlyDataMap = new HashMap<>();
+        Hashtable<Month, Double> monthlyDataMap = new Hashtable<>();
+        Month m;
+
+        // Fill in the months that have no data with 0.
+
+        for (int i = 0; i < 12; i++) {
+            monthlyDataMap.put(Month.of(i + 1), 0.0);
+        }
+
         // Create series for monthly data
-        XYChart.Series<String, Double> monthlyData = new XYChart.Series();
+        XYChart.Series<Month, Double> monthlyData = new XYChart.Series<Month, Double>();
 
         // Set chart name to current year
-        monthlyData.setName(String.valueOf(now.get(Calendar.YEAR)));
+        monthlyData.setName(String.valueOf(cal.get(Calendar.YEAR)));
 
-        List<Appointment> appt = apptDao.fetchAppointments();
+        // Get all appointments from database.
+        List<Appointment> appts = apptDao.fetchAppointments();
 
-        for (Appointment a : appt) {
+        for (Appointment appt : appts) {
             // Show only current year.
-            if (a.getEndDate().getYear() != now.get(Calendar.YEAR))
+            if (appt.getEndDate().getYear() != cal.get(Calendar.YEAR))
                 break;
+            m = appt.getEndDate().getMonth();
 
             // go through this years appointments products and calculate monthly revenue.
-            for (Product p : a.getProducts()) {
+            for (Product p : appt.getProducts()) {
+                // Calculate price of product from price and discount.
+                double price = p.getPrice() * (1 - appt.getDiscount(p).getDiscountPercent());
+                System.out.println(price);
                 // if the month is already in the map, add the price to the value.
-                Month m = a.getEndDate().getMonth();
                 if (!monthlyDataMap.containsKey(m))
-                    monthlyDataMap.put(m, p.getPrice());
-
+                    monthlyDataMap.put(m, price);
                 else
-                    monthlyDataMap.put(m, monthlyDataMap.get(m) + p.getPrice());
+                    monthlyDataMap.put(m, monthlyDataMap.get(m) + price);
             }
         }
         // If yearly data is empty, return.
         if(monthlyDataMap.isEmpty())
             return;
 
-        // Add data to series
-        for (Month m : monthlyDataMap.keySet()) {
+        // Add data to series in order of months.
+        for (int i = 0; i < 12; i++) {
+            m = Month.of(i + 1);
             monthlyData.getData().add(new XYChart.Data(m.toString(), monthlyDataMap.get(m)));
         }
 
-        barChart.getData().add(monthlyData);
+        // print out the data for debugging.
+        // for (Month mo : monthlyDataMap.keySet()) {
+        //     System.out.println(mo + " " + monthlyDataMap.get(mo));
+        // }
 
-        // set text of revenue from this month and year.
-        thisMonth.setText(String.valueOf(monthlyDataMap.get(now.get(Calendar.MONTH))));
+        // Add series to chart.
+        barChart.getData().add(monthlyData);
+        int mo = cal.get(Calendar.MONTH);
+        // set text of revenue from this and last month.
+        thisMonth.setText(String.valueOf(monthlyDataMap.get(Month.of(mo + 1))));
         
-        double total = 0;
-        // Go through the hashmap and count total revenue for year
-        for(Month m : monthlyDataMap.keySet()){
-            total += monthlyDataMap.get(m);
-        }
-        thisYear.setText(String.valueOf(total));
+
+        lastMonth.setText(String.valueOf(monthlyDataMap.get(Month.of(mo))));
     }
 }
