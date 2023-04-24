662fc86bf3c739a76fa41a7e1fb6397f388cc81d
Cl0wnFiesta <clownfiesta98@gmail.com>
2023-04-24 17:45:32 +0300

    fdrr

--- aniwash/src/main/java/aniwash/viewmodels/ShoppingCart.java
+++ aniwash/src/main/java/aniwash/viewmodels/ShoppingCart.java
@@ -20,6 +20,7 @@ public class ShoppingCart {
     public void editDiscountString(String product, String discount) {
         for (Map.Entry<Product, String> entry : productsMap.entrySet()) {
             if (entry.getKey().getName("en") == product) {
+                System.out.println("herehere");
                 productsMap.put(entry.getKey(), discount);
             }
         }
@@ -28,6 +29,7 @@ public class ShoppingCart {
     public Product getProduct(String product) {
         for (Map.Entry<Product, String> entry : productsMap.entrySet()) {
             if (entry.getKey().getName("en") == product) {
+                System.out.println("herehere");
                 return entry.getKey();
             }
         }
